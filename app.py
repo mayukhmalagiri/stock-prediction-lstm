481977d9b06d30a9359e1d07e2fd7af1fe2c78b9
Mayukhmala Giri <mayukhmala94@gmail.com>
2026-03-05 06:37:19 +0530

    Fixed prediction graph connection

--- app.py
+++ app.py
@@ -2,7 +2,11 @@
 # GRAPH SECTION (PROFESSIONAL)
 # -----------------------------------
 
-# determine past range
+# -----------------------------------
+# GRAPH SECTION (PROFESSIONAL)
+# -----------------------------------
+
+# determine past range from dropdown
 if selected_past == "6m":
     past_days = 126
 elif selected_past == "1y":
@@ -10,24 +14,28 @@ elif selected_past == "1y":
 else:
     past_days = 756
 
+# past data
 dates = df["date"].iloc[-past_days:]
 past_prices = prices.flatten()[-past_days:]
 
-# future predictions
+# predicted future values
 future_line = future_predictions.flatten()
 
+# last date of past data
 last_date = dates.iloc[-1]
 
+# generate future dates
 future_dates = pd.date_range(
     start=last_date,
     periods=len(future_line),
     freq="D"
 )
 
-# connect future to last past point
+# connect future prediction to last past point
 future_line = np.insert(future_line, 0, past_prices[-1])
 future_dates = np.insert(future_dates, 0, last_date)
 
+# past trace
 past_trace = go.Scatter(
     x=dates,
     y=past_prices,
@@ -36,6 +44,7 @@ past_trace = go.Scatter(
     line=dict(color='blue', width=3)
 )
 
+# future trace
 future_trace = go.Scatter(
     x=future_dates,
     y=future_line,
@@ -44,13 +53,13 @@ future_trace = go.Scatter(
     line=dict(color='red', width=3, dash='dash')
 )
 
+# layout
 layout = go.Layout(
     title=f"{selected_stock} Stock Price Forecast",
     xaxis=dict(title="Date"),
     yaxis=dict(title="Price per Share ($)"),
     template="plotly_white",
 
-    # vertical line where prediction starts
     shapes=[
         dict(
             type="line",
@@ -63,6 +72,7 @@ layout = go.Layout(
     ]
 )
 
+# generate graph
 fig = go.Figure(data=[past_trace, future_trace], layout=layout)
 
 graph_url = pyo.plot(fig, output_type='div', include_plotlyjs=False)
